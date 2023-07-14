422cefd32765d4a94cd767fd3050648010974e47
Anas <anasfarhad304@gmail.com>
2023-07-14 15:52:24 +0500

    get() and lazy difference

--- app/src/main/java/com/example/roomdb/data/AppContainer.kt
+++ app/src/main/java/com/example/roomdb/data/AppContainer.kt
@@ -6,11 +6,26 @@ import com.example.roomdb.data.repository.OfflineItemsRepository
 import com.example.roomdb.data.room.ItemDatabase
 
 interface AppContainer {
-    val itemsRepository : ItemRepository
+    val itemsRepository: ItemRepository
 }
 
 
-class AppDataContainer(private val context : Context) : AppContainer {
-    override val itemsRepository: ItemRepository
-        get() = OfflineItemsRepository(ItemDatabase.getDatabase(context).itemDao())
+class AppDataContainer(private val context: Context) : AppContainer {
+    /**With this declaration you can make sure that the itemsRepository property is
+     * created lazily and reuse the same instance over and over again without creating
+     * a new instance everytime you use it
+     * * */
+    override val itemsRepository: ItemRepository by lazy {
+        OfflineItemsRepository(
+            ItemDatabase.getDatabase(
+                context
+            ).itemDao()
+        )
+    }
+
+    /** The declaration of itemsRepository propperty will provide a new instance of it
+     * whenever itemsRespository is instentiated/called.
+     *
+     * override val itemsRepository: ItemRepository
+        get() = OfflineItemsRepository(ItemDatabase.getDatabase(context).itemDao())*/
 }
